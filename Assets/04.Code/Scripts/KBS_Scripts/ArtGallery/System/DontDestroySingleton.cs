c79293d58eb80496a002e4709e57dc705a3847c3
kitsune4864 <necsinete9050@gmail.com>
2024-09-02 15:46:10 +0900

    Add : asset

---
+++ Assets/04.Code/Scripts/KBS_Scripts/ArtGallery/System/DontDestroySingleton.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class DontDestroySingleton : MonoBehaviour
+{
+    public static DontDestroySingleton instance = null;
+    
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            if (instance != this)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
