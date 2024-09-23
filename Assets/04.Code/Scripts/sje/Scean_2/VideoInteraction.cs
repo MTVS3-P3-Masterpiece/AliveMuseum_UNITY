6428a057b0629800c88cdec63dba0e6b54c0ed46
ajune010 <cang6339@naver.com>
2024-09-23 12:56:26 +0900

    오브젝트 주체

--- Assets/04.Code/Scripts/sje/Scean_2/VideoInteraction.cs
+++ Assets/04.Code/Scripts/sje/Scean_2/VideoInteraction.cs
@@ -1,3 +1,113 @@
+// using UnityEngine;
+// using UnityEngine.Video;
+// using System.Collections;
+//
+// public class VideoInteraction : MonoBehaviour
+// {
+//     [System.Serializable]
+//     public class CubeVideoPair
+//     {
+//         public GameObject cube;  
+//         public VideoPlayer[] videoPlayers;   
+//         public float delay;   
+//     }
+//
+//     public CubeVideoPair[] cubeVideoPairs;  
+//     public float interactionDistance = 3.0f;   
+//     public KeyCode interactionKey = KeyCode.E;   
+//
+//     private bool[] isVideoPlaying; 
+//     private Light[] sceneLights;   // 씬의 모든 조명
+//
+//     void Start()
+//     {
+//         isVideoPlaying = new bool[cubeVideoPairs.Length];
+//
+//         // 씬에 있는 모든 Light 컴포넌트를 찾음
+//         sceneLights = FindObjectsOfType<Light>();
+//
+//         for (int i = 0; i < cubeVideoPairs.Length; i++)
+//         {
+//             foreach (VideoPlayer videoPlayer in cubeVideoPairs[i].videoPlayers)
+//             {
+//                 videoPlayer.Stop();
+//             }
+//             isVideoPlaying[i] = false;
+//         }
+//     }
+//
+//     void Update()
+//     {
+//         for (int i = 0; i < cubeVideoPairs.Length; i++)
+//         {
+//             float distance = Vector3.Distance(transform.position, cubeVideoPairs[i].cube.transform.position);
+//             
+//             if (distance <= interactionDistance && Input.GetKeyDown(interactionKey))
+//             {
+//                 if (!isVideoPlaying[i])
+//                 {
+//                     StartCoroutine(PlayVideosWithDelay(cubeVideoPairs[i].videoPlayers, cubeVideoPairs[i].delay));
+//                     TurnOffLights();  // 비디오 재생 시 조명 끄기
+//                     isVideoPlaying[i] = true;
+//                 }
+//                 else
+//                 {
+//                     foreach (VideoPlayer videoPlayer in cubeVideoPairs[i].videoPlayers)
+//                     {
+//                         videoPlayer.Stop();
+//                     }
+//                     TurnOnLights();  // 비디오 중지 시 조명 켜기
+//                     isVideoPlaying[i] = false;
+//                 }
+//             }
+//         }
+//     }
+//
+//     private IEnumerator PlayVideosWithDelay(VideoPlayer[] videoPlayers, float delay)
+//     {
+//         yield return new WaitForSeconds(delay);
+//
+//         foreach (VideoPlayer videoPlayer in videoPlayers)
+//         {
+//             videoPlayer.Play();
+//         }
+//     }
+//
+//     // 씬의 모든 조명을 끄는 함수
+//     private void TurnOffLights()
+//     {
+//         foreach (Light light in sceneLights)
+//         {
+//             light.enabled = false;
+//         }
+//     }
+//
+//     // 씬의 모든 조명을 켜는 함수
+//     private void TurnOnLights()
+//     {
+//         foreach (Light light in sceneLights)
+//         {
+//             light.enabled = true;
+//         }
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 using UnityEngine;
 using UnityEngine.Video;
 using System.Collections;
@@ -18,6 +128,7 @@ public class VideoInteraction : MonoBehaviour
 
     private bool[] isVideoPlaying; 
     private Light[] sceneLights;   // 씬의 모든 조명
+    private GameObject player; // 플레이어를 저장할 변수
 
     void Start()
     {
@@ -34,30 +145,36 @@ public class VideoInteraction : MonoBehaviour
             }
             isVideoPlaying[i] = false;
         }
+
+        // 태그가 "Player"인 오브젝트를 찾음
+        player = GameObject.FindWithTag("Player");
     }
 
     void Update()
     {
-        for (int i = 0; i < cubeVideoPairs.Length; i++)
+        if (player != null) // 플레이어가 존재하는 경우
         {
-            float distance = Vector3.Distance(transform.position, cubeVideoPairs[i].cube.transform.position);
-            
-            if (distance <= interactionDistance && Input.GetKeyDown(interactionKey))
+            for (int i = 0; i < cubeVideoPairs.Length; i++)
             {
-                if (!isVideoPlaying[i])
+                float distance = Vector3.Distance(player.transform.position, cubeVideoPairs[i].cube.transform.position);
+                
+                if (distance <= interactionDistance && Input.GetKeyDown(interactionKey))
                 {
-                    StartCoroutine(PlayVideosWithDelay(cubeVideoPairs[i].videoPlayers, cubeVideoPairs[i].delay));
-                    TurnOffLights();  // 비디오 재생 시 조명 끄기
-                    isVideoPlaying[i] = true;
-                }
-                else
-                {
-                    foreach (VideoPlayer videoPlayer in cubeVideoPairs[i].videoPlayers)
+                    if (!isVideoPlaying[i])
                     {
-                        videoPlayer.Stop();
+                        StartCoroutine(PlayVideosWithDelay(cubeVideoPairs[i].videoPlayers, cubeVideoPairs[i].delay));
+                        TurnOffLights();  // 비디오 재생 시 조명 끄기
+                        isVideoPlaying[i] = true;
                     }
-                    TurnOnLights();  // 비디오 중지 시 조명 켜기
-                    isVideoPlaying[i] = false;
+                    else
+                    {
+                        foreach (VideoPlayer videoPlayer in cubeVideoPairs[i].videoPlayers)
+                        {
+                            videoPlayer.Stop();
+                        }
+                        TurnOnLights();  // 비디오 중지 시 조명 켜기
+                        isVideoPlaying[i] = false;
+                    }
                 }
             }
         }
@@ -91,3 +208,15 @@ public class VideoInteraction : MonoBehaviour
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
