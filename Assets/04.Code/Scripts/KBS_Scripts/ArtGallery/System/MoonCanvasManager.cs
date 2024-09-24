60836af92b4243bd24541865da38a516757c3fca
kitsune4864 <necsinete9050@gmail.com>
2024-09-24 21:26:35 +0900

    add & fix : Scene Changer

--- Assets/04.Code/Scripts/KBS_Scripts/ArtGallery/System/MoonCanvasManager.cs
+++ Assets/04.Code/Scripts/KBS_Scripts/ArtGallery/System/MoonCanvasManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -6,13 +7,14 @@ public class MoonCanvasManager : MonoBehaviour
 {
     public Image BG;
     public Image EndingBG;
-    public TMP_Text guideText;
+    public TMP_Text waitText;
+    public TMP_Text pressTText;
     
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        StartCoroutine(TextChanger());
     }
 
     // Update is called once per frame
@@ -20,9 +22,16 @@ public class MoonCanvasManager : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.T))
         {
-            guideText.gameObject.SetActive(false);
             BG.gameObject.SetActive(false);
         }
+    }
+
+    IEnumerator TextChanger()
+    {
+        waitText.gameObject.SetActive(true);
+        yield return new WaitForSeconds(3f);
+        waitText.gameObject.SetActive(false);
+        pressTText.gameObject.SetActive(true);
     }
     
 }
