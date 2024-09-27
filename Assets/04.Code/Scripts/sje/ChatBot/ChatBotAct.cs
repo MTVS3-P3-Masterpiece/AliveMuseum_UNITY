6514e7fbf686e4bd345835959740a6d4a3dfe024
ajune010 <cang6339@naver.com>
2024-09-27 10:24:47 +0900

    챗봇 베타에 적용

--- Assets/04.Code/Scripts/sje/ChatBot/ChatBotAct.cs
+++ Assets/04.Code/Scripts/sje/ChatBot/ChatBotAct.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.UI;
@@ -17,7 +18,7 @@ public class ChatBotAct : MonoBehaviour
     bool chatOpen = false;
     private bool followPlayer = false;  
     private bool isChatBotVisible = true;
-    public Text textChat;
+    public TMP_Text textChat;
 
     PlayerManager playerScript;
     GameObject player;  
